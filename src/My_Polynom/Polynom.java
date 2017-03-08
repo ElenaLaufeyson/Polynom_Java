640ea6777504b0d54dd8c1f6985a49f7bbf7a26d
ElenaLaufeyson <elena_petrova98@mail.ru>
2017-03-08 21:20:14 +0300

    Update Polynom.java
    Исправила замечаниия

--- src/My_Polynom/Polynom.java
+++ src/My_Polynom/Polynom.java
@@ -12,17 +12,17 @@ public class Polynom {
 
     @Override
     public String toString() {
-        String s = new String("");
+        StringBuilder s = new StringBuilder("");
         for (int i=this.deg; i>=0; i--) {
             if (this.array[i] == 0) continue;
             if ((this.array[i] > 0) && (i!=this.deg))
-                s += "+";
-            if ((int)this.array[i] == this.array[i]) s += (int)this.array[i];
-            else s+= this.array[i];
-            if (i == 1) s+= "x";
-            else if (i != 0) s+= "x^" + i;
+                s.append("+");
+            if ((int)this.array[i] == this.array[i]) s.append((int)this.array[i]);
+            else s.append(this.array[i]);
+            if (i == 1) s.append("x");
+            else if (i != 0) s.append("x^" + i);
         }
-        return s;
+        return s.toString();
     }
 
     @Override
@@ -82,14 +82,15 @@ public class Polynom {
         return res;
     }
 
-    public void invert() {
-        for (int i=0; i<=this.deg; i++)
-            this.array[i] = -this.array[i];
+    public Polynom invert() {
+        Polynom res = new Polynom(this);
+        for (int i=0; i<=res.deg; i++)
+            res.array[i] = -res.array[i];
+        return res;
     }
 
     public Polynom dif(Polynom p2) {
-          Polynom copyp2 = new Polynom(p2);
-          copyp2.invert();
+          Polynom copyp2 = p2.invert();
           Polynom res = this.sum(copyp2);
           return res;
     }
@@ -102,18 +103,19 @@ public class Polynom {
         return result;
     }
 
-    public void cut0() {
+    private void cut0() {
         if (this.deg == 0) return;
         int i;
-        Polynom cutPol = new Polynom(this);
+//        Polynom cutPol = new Polynom(this);
         for (i = this.deg; i >= 0; i--) {
             if (this.array[i] != 0) break;
         }
         if (i == this.deg || i == -1) return;
+        double newar[] = new double[this.deg+1];
+        System.arraycopy(this.array, 0, newar, 0, this.deg+1);
         this.deg = i;
         this.array = new double[this.deg + 1];
-        System.arraycopy(cutPol.array, 0, this.array, 0, this.deg + 1);
-        System.gc();
+        System.arraycopy(newar, 0, this.array, 0, this.deg + 1);
         return;
     }
 
@@ -128,14 +130,11 @@ public class Polynom {
         return res;
     }
 
-    public Polynom div (Polynom p2, boolean resb) {
+    public Polynom div (Polynom p2) {
         if (this.equals(p2)) {
             Polynom res = new Polynom(1);
             res.array[0] = 1;
-            Polynom ost = new Polynom(1);
-            ost.array[0] = 0;
-            if (resb) return res;
-            else return ost;
+            return res;
         }
         this.cut0(); p2.cut0();
         if (p2.array[p2.deg] == 0.0) throw new IllegalArgumentException("На ноль делить нельзя!");
@@ -153,8 +152,29 @@ public class Polynom {
             ost = ost.dif(multipleres);
         }
         res.cut0();
+        return res;
+    }
+
+    public Polynom divost (Polynom p2) {
+        if (this.equals(p2)) {
+            Polynom ost = new Polynom(1);
+            ost.array[0] = 0;
+            return ost;
+        }
+        this.cut0(); p2.cut0();
+        if (p2.array[p2.deg] == 0.0) throw new IllegalArgumentException("На ноль делить нельзя!");
+        Polynom ost = new Polynom(this);
+        Polynom multipleres = new Polynom(p2.deg);
+        Polynom mnog = new Polynom(this.deg);
+        double mn;
+        while (ost.deg >= p2.deg) {
+            mn = ost.array[ost.deg] / p2.array[p2.deg];
+            Arrays.fill(mnog.array, 0);
+            mnog.array[ost.deg - p2.deg] = mn;
+            multipleres = p2.multipl(mnog);
+            ost = ost.dif(multipleres);
+        }
         ost.cut0();
-        if (resb) return res;
-        else return ost;
+        return ost;
     }
 }
